227e8b5949b85844a39fe7643d5be51000e6632d
iomonad <iomonad@riseup.net>
2025-09-19 16:50:56 +0200

    feature[algo] added `similar-artists` first algorithm

--- src/discogs/algos.clj
+++ src/discogs/algos.clj
@@ -1,9 +1,39 @@
 (ns discogs.algos
   "Some algorithms made using Discogs data"
-  (:require [discogs.database :refer [search]]))
+  (:require [discogs.database :refer [search
+                                      get-artist]]))
 
 (defn search-artists->ids
   "Search for artists name, and return matching as id"
+  {:added "0.1.3"}
   ([client artist-name]
    (let [results (search client {:artist artist-name})]
      (into #{} (map :id results)))))
+
+(defn similar-artists
+  "Search for similar artists
+
+   Return a list of map with Artist name and id, NOT the
+   full artist resource"
+  {:added "0.1.4"}
+  ([client artist-id]
+   (similar-artists client artist-id {:depth 1}))
+  ([client artist-id {:keys [depth]}]
+   (let [{:keys [members aliases groups] :as artist} (get-artist client artist-id)]
+     (cond->> []
+       (seq aliases) (concat aliases)
+
+       (seq groups)  (concat groups)
+
+       (seq members)
+       ((fn add-members [res]
+          (when-let
+           [mg (seq
+                (mapcat
+                 (fn [{:keys [id]}]
+                   (let [{:keys [groups aliases]} (get-artist client id)]
+                     (concat groups aliases)))
+                 members))]
+            (concat res mg))))
+       :always (sort-by :name)
+       :always (into #{})))))
